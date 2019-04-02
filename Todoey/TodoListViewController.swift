0c50261ccd28ddecc2062fd32c3400f72a09b822
Sam Bush <sambush@macs-computer-2.local>
2019-04-01 20:12:32 -0400

    Initial Commit

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Sam Bush on 3/30/19.
+//  Copyright © 2019 Sam Bush. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Walk Dogs", "Empty Dishwasher", "Clean Patio"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell")!
+        
+        let text = itemArray[indexPath.row]
+        cell.textLabel?.text = text
+        
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        //adds check mark on item. 
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+
+        }
+        
+        //removes row highlight
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
